45afa44c4730c949a5d6c6fbbbfbb9ec9d8db6ee
Abdoelabassi <abderrezaq@outlook.com>
2022-05-20 21:26:33 +0100

    first hands on

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+
+use::std::env::{args, Args};
+
+fn main() {
+
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+
+    let second = args.nth(0).unwrap();
+
+
+    let first_number = first.parse::<f32>().unwrap();
+
+    let second_number = second.parse::<f32>().unwrap();
+
+    let result = operate(operator, first_number, second_number);
+
+    println!("{:?}", output(first_number, operator, second_number, result));
+}
+
+fn operate(op:char, x:f32, y:f32)-> f32{
+
+    match op {
+        '+' => x + y,
+        '-' => x - y,
+        '*' => x*y,
+        '/' => x/y,
+        _ => 0.0
+    }
+}
+
+
+fn output(x:f32, op:char, y:f32, res:f32)->String {
+
+    format!("{} {} {} = {} ", x, op, y, res)
+}
